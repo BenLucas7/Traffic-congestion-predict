5d6b1b204371ef493ecaea552f294889a19ce02f
BenLucas <yoga867530010@icloud.com>
2019-04-28 13:14:29 +0800

    add LSTM model

---
+++ SVR/lstm.py
@@ -0,0 +1,11 @@
+import gc
+import datetime
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import keras
+from keras.models import Sequential
+from keras.layers import Activation, Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
